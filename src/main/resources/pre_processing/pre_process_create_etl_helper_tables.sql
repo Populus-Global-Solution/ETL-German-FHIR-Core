ab13a3bd4210198b4c73a323baa007e965caed83
LJNIC <liamreadsspam@gmail.com>
2023-07-27 12:15:45 -0300

    Make ID columns generated, create mapping lookup table

--- src/main/resources/pre_processing/pre_process_create_etl_helper_tables.sql
+++ src/main/resources/pre_processing/pre_process_create_etl_helper_tables.sql
@@ -64,3 +64,26 @@ CREATE materialized VIEW IF NOT EXISTS cds_etl_helper.snomed_race_standard_looku
   AND    c1.domain_id::text = 'Race'::text WITH data;
 END
 $$;
+
+DO $$
+BEGIN
+CREATE MATERIALIZED VIEW IF NOT EXISTS cds_etl_helper.icd_snomed_domain_lookup AS
+  SELECT c1.concept_code AS icd_gm_code,
+         c1.concept_id AS icd_gm_concept_id,
+         cr.concept_id_2 AS snomed_concept_id,
+         c2.domain_id AS snomed_domain_id,
+         c3.domain_concept_id AS snomed_domain_concept_id,
+         c1.valid_start_date AS icd_gm_valid_start_date,
+         c1.valid_end_date AS icd_gm_valid_end_date
+  FROM cdm.concept c1
+  JOIN cdm.concept_relationship cr
+  ON   c1.concept_id = cr.concept_id_1
+  JOIN cdm.concept c2
+  ON   cr.concept_id_2 = c2.concept_id
+  JOIN cdm.domain c3
+  ON c2.domain_id = c3.domain_id
+  WHERE 1=1
+    AND c1.vocabulary_id = 'ICD10GM'
+    AND cr.relationship_id = 'Maps to';
+END
+$$;
